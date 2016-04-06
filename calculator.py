c6a00bb5c37a7bd4e7cc5aea2cb071d18d8f6ef6
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 16:51:35 -0700

    done but has bugs

--- calculator.py
+++ calculator.py
@@ -7,7 +7,30 @@
 
 def calc():
     """Take input and decide which function to call to evaluate"""
-    while not_q:
-        input = open('file_name')
-        tokens = input.split(" ") #tokenizing the input aka pow 3 5 into [pow, 3, 5]
-        
+    while True:
+        print 'welcome to the calculator!'
+        input = raw_input("Please give me a prefix expression. ")
+        token = input.split(" ") #tokenizing the input aka pow 3 5 into [pow, 3, 5]
+        # print token, testing to see what our tokens looked like
+        if token[0] == 'q':
+            return None
+        elif token[0] == '+':
+            return token[1] + token[2]
+        elif token[0] == '-':
+            return token[1] - token[2]
+        elif token[0] == '*':
+            return token[1] * token[2]
+        elif token[0] == '/':
+            return float(token[1]) / token[2] 
+        elif token[0] == 'square':
+            return token[1] ** 2
+        elif token[0] == 'cube':
+            return token[1]** 3
+        elif token[0] == 'pow':
+            return token[1] ** token[2]
+        elif token[0] == 'mod':
+            return token[1] % token[2]
+        else: 
+            print 'That was not the correct prefix expression!'
+
+calc()
